24010765095fd5044cc1630e0f51d39850d0bb27
vegardbb <vegardbb@stud.ntnu.no>
2016-04-15 09:50:08 +0200

    Add textureatlas

--- core/src/com/snakeladders/game/Assets.java
+++ core/src/com/snakeladders/game/Assets.java
@@ -17,18 +17,18 @@ public class Assets { // Each asset is publically available, visible to EveryOne
     public static Texture backgroundTexture; // Used in the main menu
     public static Animation pieceAni; // Not used per now, intended as animation w/ movement only. No texturechange.
     public static Texture boardTexture; // The board, covering the screen
-    // TODO: Piece - actors extends Actor and has their own asset texture laoded from the Assets - folder, depending on their int-id, so no piece texture here.
 
     // For generating buttons in the menu UI
-    public static TextButton.TextButtonStyle textButtonStyle;
-    public static BitmapFont font12;
-    public static TextureAtlas buttonAtlas;
+    public static TextButtonStyle textButtonStyle;
+    public static BitmapFont font;
+    public static Skin skin;
+    public static TextureAtlas buttonAtlas;    
 
     private static void generateFont() {
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-Regular.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 12;
-        font12 = generator.generateFont(parameter); // font size 12 pixels
+        font = generator.generateFont(parameter); // font size 12 pixels
         generator.dispose(); // don't forget to dispose to avoid memory leaks!
     }
 
@@ -36,6 +36,14 @@ public class Assets { // Each asset is publically available, visible to EveryOne
         // Implements asset loading.
         generateFont();
         backgroundTexture = new Texture(Gdx.files.internal("splash.png")); // For the splash screen
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("ui-orange.atlas"));
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("button_01");
+        textButtonStyle.down = skin.getDrawable("button_02");
+        textButtonStyle.checked = skin.getDrawable("checkbox_on");
         }
          // Animation, Texture, Textureregion, TextureAtlas. 
 }
