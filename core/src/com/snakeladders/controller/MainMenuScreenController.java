19e5cd1d06f418d47e0ed76ec766c17a6caa1d2f
Stian Torjussen <stiant92@gmail.com>
2016-04-15 13:09:26 +0200

    Adding newGame and exit for MainMenuController

--- core/src/com/snakeladders/controller/MainMenuScreenController.java
+++ core/src/com/snakeladders/controller/MainMenuScreenController.java
@@ -1,11 +1,30 @@
 package com.snakeladders.controller;
 
+import com.snakeladders.view.PlayerInfoScreen;
+import com.snakeladders.controller.SnakeLadders;
+
 public class MainMenuScreenController {
+    SnakeLadders game;
+
+    public MainMenuScreenController(SnakeLadders game) {
+        this.game = game;
+    }
+
     /**
 	This class controls the input from the MainMenuScreen
-	TODO: Insert getters for assets in Assets class
 	TODO: Implement exitbutton method
 	TODO: Implement newGameButton
     **/
+    public void MainMenuScreenController(SnakeLadders game){
+
+    }
+
+	public void newGame(){
+        game.setScreen(new PlayerInfoScreen(game));
+	}
+
+	public void exit(){
+        //game.exit();
+	}
 }
 
