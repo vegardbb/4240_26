ea99fc0981853ee9ac6333635d8f2adb022cbe23
Stian Torjussen <stiant92@gmail.com>
2016-04-15 13:10:08 +0200

    Generating PlayerInfoScreen

--- core/src/com/snakeladders/view/PlayerInfoScreen.java
+++ core/src/com/snakeladders/view/PlayerInfoScreen.java
@@ -1,7 +1,49 @@
 package com.snakeladders.view;
 
+import com.badlogic.gdx.Screen;
+import com.snakeladders.controller.SnakeLadders;
+
 /**
  * Created by HellGoose-Laptop on 4/15/2016.
  */
-public class PlayerInfoScreen {
+public class PlayerInfoScreen implements Screen {
+    private SnakeLadders game;
+    public PlayerInfoScreen(SnakeLadders game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
