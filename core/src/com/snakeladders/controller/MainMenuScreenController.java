8cf913765d256a7a6b42c1fd94e794e334c84dcd
Stian Torjussen <stiant92@gmail.com>
2016-04-24 15:01:54 +0200

    Controller cleanup

--- core/src/com/snakeladders/controller/MainMenuScreenController.java
+++ core/src/com/snakeladders/controller/MainMenuScreenController.java
@@ -3,11 +3,10 @@ package com.snakeladders.controller;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.snakeladders.model.Assets; // The Assets class
+import com.snakeladders.model.Assets;
 import com.snakeladders.view.GameScreen;
 
 public class MainMenuScreenController {
@@ -17,45 +16,20 @@ public class MainMenuScreenController {
         this.game = game;
     }
 
-    /**
-	Initialises the game screen.
-    **/
-	public void newGame(int playerCount){
-        game.setScreen(new GameScreen(game, playerCount));
-	}
+	// Initialises the game screen.
+	public void newGame(int playerCount){ game.setScreen(new GameScreen(game, playerCount)); }
 
+    // Exit the game
     public void exit(){
         game.dispose();
         Gdx.app.exit();
 	}
 
     // The following getter emthods deliver the textures, the skin, the styles and the font needed to draw the Main Menu screen
-    public static Skin getSkin() {
-        return Assets.getSkin();
-    }
-
-    public static Texture getBackgroundTexture() {
-        return Assets.getBackgroundTexture();
-    }
-
-    public static Texture getBoardTexture() {
-        return Assets.getBoardTexture();
-    }
-
-    public static TextButton.TextButtonStyle getTextButtonStyle() {
-        return Assets.getTextButtonStyle();
-    }
-
-    public static BitmapFont getFont() {
-        return Assets.getFont();
-    }
-
-    public static TextureAtlas getButtonAtlas() {
-        return Assets.getButtonAtlas();
-    }
-
-    public static Slider.SliderStyle getSliderStyle(){
-        return Assets.getSliderStyle();
-    }
+    public static Skin getSkin() { return Assets.getSkin(); }
+    public static BitmapFont getFont() { return Assets.getFont(); }
+    public static Slider.SliderStyle getSliderStyle(){ return Assets.getSliderStyle(); }
+    public static Texture getBackgroundTexture() { return Assets.getBackgroundTexture(); }
+    public static TextButton.TextButtonStyle getTextButtonStyle() { return Assets.getTextButtonStyle(); }
 }
 
